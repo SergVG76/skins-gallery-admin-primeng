91960a4155274ee2e01730d49cad497533eb9ccf
SergVG76 <sergunalpha@gmail.com>
2021-04-15 13:25:01 +0300

    3 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,6 @@ import { DataService, Skin } from "./data.service";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
-  styles: [
-    `
-      :host ::ng-deep .p-dialog .product-image {
-        width: 150px;
-        margin: 0 auto 2rem auto;
-        display: block;
-      }
-    `
-  ]
 })
 export class AppComponent implements OnInit {
   skins: Skin[];
